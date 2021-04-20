f1d9f76c54f0b9cdb63fbae645a453b8c281ec60
norovdenis <norovdenis96@gmail.com>
2021-04-20 19:22:53 +0300

    Добавьте файлы проекта.

---
+++ SeaBattle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle {
+    class Program {
+       static void Main(string[] args) {
+            int height;
+            int width;
+            bool isTryParse = true;
+            Console.Write("Строк: ");
+            isTryParse &= int.TryParse(Console.ReadLine(), out height);
+            Console.Write("Столбцов: ");
+            isTryParse &= int.TryParse(Console.ReadLine(), out width);
+
+            if (!isTryParse) {
+                Console.WriteLine("Значения введены не корректно");
+                return;
+            }
+            if (height < 4 & width < 4) {
+                Console.WriteLine("Слишком маленькие значения");
+                return;
+            }
+
+            PlacementOfShips ships = new PlacementOfShips(height, width);
+            for (int i = 4; i > 0; --i) {
+                for (int j = i; j < 5; ++j) {
+                     if (!ships.AddShip(i)) {
+                        Console.WriteLine("Не удалось разместить все корабли.");
+                        return;
+                    }
+                }
+            }
+            ships.ShowCellArray();
+        }
+    }
+}
